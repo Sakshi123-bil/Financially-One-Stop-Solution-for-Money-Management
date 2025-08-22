7013499e1ea72935b8d2421734bdb3527ef145ef
Sakshi Padamwar <sakshipadamwar2002@gmail.com>
2025-08-22 15:00:46 +0530

    login page UI

--- frontend/expense-tracker/src/components/layout/AuthLayout.jsx
+++ frontend/expense-tracker/src/components/layout/AuthLayout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CARA_2 from "../../assets/Images/card2.png";
+import {LuTrendingUpDown} from "react-icons/lu";
 const AuthLayout = ({ children }) => {
     return (
         <div className="flex">
@@ -11,7 +12,14 @@ const AuthLayout = ({ children }) => {
                 <div className="w-48 h-48 rounded-[40px] bg-purple-600 absolute -top-7 -left-5"></div>
                 <div className="w-48 h-56 rounded-[40px] border-[20px] border-fuchsia-600 absolute top-[30%] -right-10"></div>
                 <div className="w-48 h-48 rounded-[40px] bg-violet-500 absolute -bottom-7 -left-5"></div>
-
+                <div className="grid grid-cols z-20">
+                <StatsInfoCard
+                icon={<LuTrendingUpDown/>}
+                label="Track Your Income & Expenses"
+                value="43,000"
+                color="bg-primary"
+                />
+                </div>
                 <img
                     src={CARA_2}
                     className="w-64 lg:w-[90%] absolute bottom-10 shadow-lg shadow-blue-400/15"
@@ -22,3 +30,17 @@ const AuthLayout = ({ children }) => {
 }
 
 export default AuthLayout;
+
+const StatsInfoCard = ({icon , label ,value , color}) =>{
+    return(
+        <div className="flex gap-6 bg-white p-4 rounded-xl shadow-md shadow-purple-400/10 border border-gray-200/15 z-10">
+            <div className={`w-12 h-12 flex items-center justify-center text-[26px] text-white ${color} rounded-full drop-shadow-xl`}>
+               {icon}
+            </div>
+            <div>
+                <h6 className="text-xs text-gray-500 mb-1">{label}</h6>
+                <span className="text-[20px]">${value}</span>
+            </div>
+        </div>
+    )
+}
